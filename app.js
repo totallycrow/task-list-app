1e3beb785f8a976e48223ac40ebd01b3600b7234
Adrian Gajda <adrian.gajda@hotmail.com>
2019-09-16 22:19:40 +0100

    add delete & filter tasks functionality

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Define UI Vars
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('clear-tasks');
+const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
@@ -11,6 +11,14 @@ loadEventListeners();
 function loadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter Tasks Event
+    filter.addEventListener('keyup', filterTasks);
+
+
 }
 
 // Add Task
@@ -46,4 +54,40 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are You Sure?')) {
+            e.target.parentElement.parentElement.remove();
+
+        }
+
+    }
+
+}
+
+// Clear Tasks
+
+function clearTasks() {
+    // taskList.innerHTML = '';
+
+    // Faster way
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function (task) {
+        const item = task.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+
 }
